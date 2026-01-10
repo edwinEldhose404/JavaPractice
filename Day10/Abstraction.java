b379710dd9d333d310cc2ab8d1c24d35dacb352d
edwineldhose3 <edwineldhose3@gmail.com>
2026-01-10 11:35:59 +0530

    abstraction

---
+++ Day10/Abstraction.java
@@ -0,0 +1,21 @@
+package Day10;
+
+public class Abstraction {
+    //Abstract class
+        //abstract class <class_name>{
+        //abstract <AM> <returnType> method(<formal_args>);
+        // }
+            //"abstract" and <AM> can be interchanged
+
+        //abstact classes have no body and do nothing which is the opposite of concrete methods
+        //Concrete methods are fully implemented methods with a header, body and purpose
+
+        //Upcasting can be used with abstract classes to make the class use child class methods
+
+        //When a abstract class has a subclass abstract class, then the subclass wont have 
+        // implementation
+        //Concrete classes are required to implement the methods of the abstract classes 
+        //that it inherits from
+
+        
+}
---
+++ Day10/Polymorphism.java
@@ -0,0 +1,13 @@
+package Day10;
+
+public class Polymorphism {
+    //Multiple methods with same name which has multiple behaviours
+    //Types of compile-time polymorphism
+        //Overloading has two types
+            //Method && Constructor
+                //Non-parameterized and parameterized method name should be the same
+                //Can overload static, private, and normal methods
+                //FINAL CAN BE OVERLOADED BUT NOT OVERIDDEN
+        //Shadowing
+            //Method && Variable
+}
